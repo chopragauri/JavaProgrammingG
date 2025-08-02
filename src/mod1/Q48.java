2888129fd636b1bafdc4f72f97854bab02cb11f1
chopragauri <chopra.gauri06@gmail.com>
2025-08-02 20:53:32 +0530

    code 47-51

---
+++ src/mod1/Q48.java
@@ -0,0 +1,11 @@
+package mod1;
+
+public class Q48 {
+    public static void main(String[] args) {
+        String s1= "Messi";
+        int count=0;
+        for(int i=0;i<s1.length();i++){
+
+        }
+    }
+}
