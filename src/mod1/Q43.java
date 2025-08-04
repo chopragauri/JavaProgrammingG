de3838d87d6eb656471fc3a29b5cc35a716a6785
chopragauri <chopra.gauri06@gmail.com>
2025-08-05 00:18:17 +0530

    codes m1

---
+++ src/mod1/Q43.java
@@ -0,0 +1,20 @@
+package mod1;
+class NumberPrinter {
+    void display(int num) {
+        System.out.println("Number from parent: " + num);
+    }
+}
+class SquarePrinter extends NumberPrinter {
+    @Override
+    void display(int num) {
+        super.display(num);
+        System.out.println("Square from child: " + (num * num));
+    }
+}
+public class Q43 {
+        public static void main(String[] args) {
+            SquarePrinter printer = new SquarePrinter();
+            printer.display(7);
+        }
+    }
+
